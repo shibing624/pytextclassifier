698e85129502aa20ac6adbc8bf42475f0fa3e7be
xuming <507153809@qq.com>
2017-05-03 10:57:45 +0800

    add NN classifier. xuming 20170503

---
+++ neuralnetwork.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@description: 用库工具实现神经网络分类器
+@author:XuMing
+"""
+from __future__ import print_function  # 兼容python3的print写法
+from __future__ import unicode_literals  # 兼容python3的编码处理
+
+
+
+
