812ea5e8b308b3196d92ba3719248bb19714444a
xuming06 <507153809@qq.com>
2018-03-28 10:02:54 +0800

    add xgb classification.

--- classifier/lr.py
+++ classifier/lr.py
@@ -4,14 +4,15 @@
 import os
 from codecs import open
 
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 
 from util import load_pkl, dump_pkl
 
 
 def data_reader(path):
     label_list, content_list = [], []
-    with open(path, "r", encoding="gb18030") as fp:
+    with open(path, "r", encoding="utf-8") as fp:
         for line in fp:
             parts = line.strip().split('\t')
             if parts and len(parts) > 1:
@@ -24,8 +25,9 @@ def tfidf(data_set, space_path):
     if os.path.exists(space_path):
         tfidf_space = load_pkl(space_path)
     else:
-        transformer = TfidfVectorizer()
-        tfidf_space = transformer.fit_transform(data_set)
+        vectorizer = CountVectorizer()
+        transformer = TfidfTransformer()
+        tfidf_space = transformer.fit_transform(vectorizer.fit_transform(data_set))
         dump_pkl(tfidf_space, space_path)
     print('tfidf shape:', tfidf_space.shape)
     return tfidf_space
@@ -88,14 +90,14 @@ def plot_pr(auc_score, precision, recall, label=None):
     pylab.fill_between(recall, precision, alpha=0.5)
     pylab.grid(True, linestyle='-', color='0.75')
     pylab.plot(recall, precision, lw=1)
-    pylab.savefig('data/R_P.png')
+    pylab.savefig('../data/risk/R_P.png')
 
 
 if __name__ == '__main__':
     # data
-    train_data_path = "data/train/train_seg.txt"  # 输入的文件
-    test_data_path = "data/test/test_seg.txt"  # 输入的文件
-    space_path = "data/tfidf.dat"  # 输出的文件
+    train_data_path = "../data/risk/train_seg.txt"  # 输入的文件
+    test_data_path = "../data/risk/test_seg.txt"  # 输入的文件
+    space_path = "../data/risk/tfidf.dat"  # 输出的文件
     train_set, train_label = data_reader(train_data_path)
     test_set, test_label = data_reader(test_data_path)
 
