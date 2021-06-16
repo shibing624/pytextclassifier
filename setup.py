f210298a8b24e9f70466a6be14574f7103f2b5ef
shibing624 <shibing624@126.com>
2021-06-16 16:01:03 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing(xuming624@qq.com)
+@description: 
+"""
+from __future__ import print_function
+
+import sys
+
+from setuptools import setup, find_packages
+
+from pytextclassifier import __version__
+
+if sys.version_info < (3,):
+    sys.exit('Sorry, Python3 is required for dialogbot.')
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+with open('LICENSE', 'r', encoding='utf-8') as f:
+    license = f.read()
+
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    reqs = f.read()
+
+setup(
+    name='pytextclassifier',
+    version=__version__,
+    description='Text Classifier, Text Classification',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='XuMing',
+    author_email='xuming624@qq.com',
+    url='https://github.com/shibing624/pytextclassifier',
+    license="Apache 2.0",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Natural Language :: Chinese (Traditional)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Indexing',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    keywords='pytextclassifier,textclassifier,classifier,textclassification',
+    install_requires=reqs.strip().split('\n'),
+    packages=find_packages(exclude=['tests']),
+    package_dir={'pytextclassifier': 'pytextclassifier'},
+    package_data={
+        'pytextclassifier': ['*.*', 'LICENSE', 'README.*', 'data/*', 'utils/*',
+                             'models/*', ],
+    },
+    test_suite='tests',
+)
