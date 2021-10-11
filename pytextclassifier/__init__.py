a373856d013ca8b7cfc67c22583b121a7fbd9ce0
shibing624 <shibing624@126.com>
2021-10-11 11:50:51 +0800

    add __version__

--- pytextclassifier/__init__.py
+++ pytextclassifier/__init__.py
@@ -3,6 +3,7 @@
 @author:XuMing(xuming624@qq.com)
 @description: 
 """
+__version__ = "1.0.4"
 
 from pytextclassifier.textclassifier import TextClassifier, load_data
 from pytextclassifier.textcluster import TextCluster
