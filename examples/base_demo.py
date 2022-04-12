e9406ac766cd89b29058c2cd103ed724a04b7bf3
shibing624 <shibing624@126.com>
2022-04-12 17:42:33 +0800

    update demo.

--- examples/base_demo.py
+++ examples/base_demo.py
@@ -14,7 +14,7 @@ logger.add(sys.stderr, level="INFO")  # 设置log级别
 
 if __name__ == '__main__':
     m = ClassicClassifier(model_dir='models/lr-english-toy', model_name_or_model='lr')
-    # model_name is choose classifier, default lr, support lr, random_forest, decision_tree, knn, bayes, svm, xgboost
+    # ClassicClassifier model name support lr, random_forest, decision_tree, knn, bayes, svm, xgboost
     print(m)
     data = [
         ('education', 'Student debt to cost Britain billions within decades'),
