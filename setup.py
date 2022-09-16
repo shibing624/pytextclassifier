72b6d36aa17ac4efc016f3c9c503a8afddf861b9
shibing624 <shibing624@126.com>
2022-09-16 20:35:11 +0800

    update setup.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 """
 from setuptools import setup, find_packages
 
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 
 with open('README.md', 'r', encoding='utf-8') as f:
     readme = f.read()
@@ -37,6 +37,7 @@ setup(
         "scikit-learn",
         "pandas",
         "numpy",
+        "transformers",
     ],
     packages=find_packages(exclude=['tests']),
     package_dir={'pytextclassifier': 'pytextclassifier'},
