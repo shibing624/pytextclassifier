c83aab32f32198295bfbe1fd91251dc22f8980e5
shibing624 <shibing624@126.com>
2022-12-12 16:17:07 +0800

    update lazy load no dev data.

--- pytextclassifier/bert_classifier.py
+++ pytextclassifier/bert_classifier.py
@@ -133,6 +133,7 @@ class BertClassifier(ClassifierABC):
             dev_data = None
         if self.train_args.lazy_loading:
             train_data = data_list_or_path
+            dev_data = None
         logger.debug(f"train_data size: {len(train_data)}")
         logger.debug(f'train_data sample:\n{train_data[:3]}')
         # train model
