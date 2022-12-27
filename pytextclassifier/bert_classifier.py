a2181f728c280d890fd21ab11e3ad2d5774b6294
shibing624 <shibing624@126.com>
2022-12-28 00:27:47 +0800

    update labels dev2

--- pytextclassifier/bert_classifier.py
+++ pytextclassifier/bert_classifier.py
@@ -131,14 +131,14 @@ class BertClassifier(ClassifierABC):
                 dev_data = None
         logger.debug(f"train_data size: {len(train_data)}")
         logger.debug(f'train_data sample:\n{train_data[:3]}')
-        if dev_data:
+        if dev_data is not None and dev.size:
             logger.debug(f"dev_data size: {len(dev_data)}")
             logger.debug(f'dev_data sample:\n{dev_data[:3]}')
         # train model
         if self.train_args.lazy_loading:
             train_data = data_list_or_path
             dev_data = dev_data_list_or_path
-        if dev_data:
+        if dev_data is not None:
             self.model.train_model(
                 train_data, eval_df=dev_data,
                 args={'labels_map': labels_map, 'labels_list': labels_list}
